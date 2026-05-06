beec2a42b649519b7558e6f250902b28a976cf6c
BBarbosaa <Bruno.Rocha.T0133593@edu.atec.pt>
2026-05-06 17:47:15 +0100

    Criação das classes Secretaria e Diretor- Closes #2

--- ADOSMELHORES/Data/Empresa/Diretor.cs
+++ ADOSMELHORES/Data/Empresa/Diretor.cs
@@ -1,6 +1,18 @@
-﻿namespace ADOSMELHORES.Data.Empresa
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADOSMELHORES.Data.Empresa
 {
     public class Diretor : Funcionario
     {
+        public bool IsencaoHorario { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Salario { get; set; }
+
+        public decimal BonusMensal { get; set; } 
+
+        public bool CarroEmpresa { get; set; }
+
+        public ICollection<Secretaria> Secretarias { get; set; } = new List<Secretaria>();
     }
 }
--- ADOSMELHORES/Data/Empresa/Secretaria.cs
+++ ADOSMELHORES/Data/Empresa/Secretaria.cs
@@ -1,6 +1,15 @@
-﻿namespace ADOSMELHORES.Data.Empresa
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADOSMELHORES.Data.Empresa
 {
     public class Secretaria : Funcionario
     {
+        [Required]
+
+        public string Area { get; set; } = string.Empty;
+
+        public int DiretorId { get; set; }
+
+        public Diretor? DiretoQueReporta { get; set; }
     }
 }
