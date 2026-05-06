fcd80fd7374738f22f7f869a2f13e3a5d5939050
BBarbosaa <Bruno.Rocha.T0133593@edu.atec.pt>
2026-05-06 17:50:29 +0100

    Revert "Criação das classes Secretaria e Diretor- Closes #2"
    This reverts commit beec2a42b649519b7558e6f250902b28a976cf6c.

--- ADOSMELHORES/Data/Empresa/Secretaria.cs
+++ ADOSMELHORES/Data/Empresa/Secretaria.cs
@@ -1,15 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ADOSMELHORES.Data.Empresa
+﻿namespace ADOSMELHORES.Data.Empresa
 {
     public class Secretaria : Funcionario
     {
-        [Required]
-
-        public string Area { get; set; } = string.Empty;
-
-        public int DiretorId { get; set; }
-
-        public Diretor? DiretoQueReporta { get; set; }
     }
 }
