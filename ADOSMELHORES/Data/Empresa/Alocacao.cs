478d1708bb8e12445ff1c77edbb75c490ea79392
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2026-05-08 10:02:06 +0100

    feat: Classe Alocacao

---
+++ ADOSMELHORES/Data/Empresa/Alocacao.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADOSMELHORES.Data.Empresa
+{
+    public class Alocacao
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int? FormadorId { get; set; }
+        public Formador? Formador { get; set; }
+
+        public decimal ValorHoraNoMomento { get; set; } // Importante para histórico de preços
+
+        [Required]
+        public string DescricaoFormacao { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DataInicio { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DataFim { get; set; }
+
+    }
+}
